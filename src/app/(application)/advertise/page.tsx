26deadad18411b0d4e779a5316db07d3624daa48
codezera <codezera3@gmail.com>
2023-12-10 17:19:26 +0530

    removed everything from root pages

--- src/app/(application)/advertise/page.tsx
+++ src/app/(application)/advertise/page.tsx
@@ -1,31 +1,12 @@
 'use client'
 
 import React from 'react'
-import {
-  AmenitiesForm,
-  BasicDetailsForm,
-  CallPreferenceForm,
-  LocationDetailsForm,
-  ProjectStatusForm,
-  PropertyDetailsForm,
-  RentPropertyDetailsForm
-} from './_form'
 
 const Page = () => {
-  const storeValues = (step: string, values: any) => {
-    localStorage.setItem(`advertise/${step}`, JSON.stringify(values))
-  }
 
   return (
     <section className="flex items-start gap-10 p-10">
-      <BasicDetailsForm onSave={storeValues} />
-      <PropertyDetailsForm onSave={storeValues} />
-      <RentPropertyDetailsForm onSave={storeValues} />
-      <LocationDetailsForm onSave={storeValues} />
-      <AmenitiesForm onSave={storeValues} />
-      <ProjectStatusForm onSave={storeValues} />
-      {/* TODO DOCUMENTS FORM */}
-      <CallPreferenceForm onSave={storeValues} />
+      advertise page
     </section>
   )
 }
--- src/app/(application)/mortgage/page.tsx
+++ src/app/(application)/mortgage/page.tsx
@@ -1,18 +1,12 @@
 "use client"
 
 import React from 'react'
-import PersonalDetailsForm from './_form/personal-details-form'
-import IncomeDetailsForm from './_form/income-details-form'
 
 const Page = () => {
-  const storeValues = (step: string, values: any) => {
-    localStorage.setItem(step, JSON.stringify(values))
-  }
 
   return (
     <section className="flex gap-16 p-4">
-      <PersonalDetailsForm onSave={storeValues} />
-      <IncomeDetailsForm onSave={storeValues} />
+      mortgage page
     </section>
   )
 }
