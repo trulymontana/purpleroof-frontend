b82e842ca64038ec00e43cc7aaa5c836c7e801f4
personal <mohammadfaisal1011@gmail.com>
2023-12-10 18:23:49 +0600

    removed test

--- src/data/hooks/useFileClient.ts
+++ src/data/hooks/useFileClient.ts
@@ -1,8 +1,4 @@
-import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query'
-import { userClient } from '../clients/usersClient'
-import { ApiEndpoints } from '@/constants/api'
-
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 import { fileClient } from './../../data/clients/fileClient'
 import { PreSignedFile } from '@/constants/types'
 
@@ -23,8 +19,12 @@ export const useFileUploader = (folder: string, fileName: string) => {
       .then((res) => {
         console.log('file uploaded successfully')
       })
-      .catch((err) => console.log(err))
+      .catch((err) => {
+        setLoading(false)
+        console.log(err)
+      })
       .finally(() => setLoading(false))
+
     const objectUrl = `https://${S3_BUCKET_NAME}.s3.amazonaws.com/${folder}/${fileName}-${file.name}`
     return objectUrl
   }
@@ -41,55 +41,3 @@ export const useFileDeleter = (folder: string, fileName: string) => {
   }
   return { deleteFile, loading }
 }
-
-// export function useGetPreSignedUrl() {
-//   const { isLoading, data } = useQuery({
-//     queryKey: [ApiEndpoints.FILES],
-//     queryFn: () => userClient.all()
-//   })
-
-//   return { users: data?.data, loading: isLoading }
-// }
-
-// export const useUpdateOpinionMutation = () => {
-//   const queryClient = useQueryClient()
-//   const navigate = useNavigate()
-//   return useMutation(opinionClient.update, {
-//     onSuccess: () => {
-//       toast.success('Opinion Successfully Updated')
-//       navigate(AppRoutes.OPINION_EDITOR)
-//     },
-//     onSettled: () => {
-//       queryClient.invalidateQueries(ApiEndpoints.USERS)
-//     },
-//   })
-// }
-
-// export const useCreateOpinionMutation = () => {
-//   const queryClient = useQueryClient()
-//   const navigate = useNavigate()
-//   return useMutation(opinionClient.create, {
-//     onSuccess: (data) => {
-//       toast.success('Opinion Successfully Created')
-//       navigate(AppRoutes.OPINION_EDITOR)
-//     },
-//     onSettled: () => {
-//       queryClient.invalidateQueries(ApiEndpoints.OPINION)
-//     },
-//   })
-// }
-
-// export const useDeleteOpinionMutation = () => {
-//   const queryClient = useQueryClient()
-//   const navigate = useNavigate()
-//   return useMutation(opinionClient.delete, {
-//     onSuccess: (data) => {
-//       toast.success('Opinion and its contents are successfully deleted!')
-//       navigate(AppRoutes.OPINION_EDITOR)
-//       queryClient.refetchQueries(ApiEndpoints.OPINION)
-//     },
-//     onSettled: async () => {
-//       queryClient.invalidateQueries(ApiEndpoints.OPINION)
-//     },
-//   })
-// }
