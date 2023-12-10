9b2e46ceb15934ffc7c2a2d7271b7108e6a33b48
codezera <codezera3@gmail.com>
2023-12-10 17:12:22 +0530

    removed localstorage from advertise root page

--- src/app/(application)/advertise/page.tsx
+++ src/app/(application)/advertise/page.tsx
@@ -16,26 +16,11 @@ const Page = () => {
     localStorage.setItem(`advertise/${step}`, JSON.stringify(values))
   }
 
-  const createAdvertisement = () => {
-    let allData: any = {}
-    if (localStorage.length > 0) {
-      for (let i = 0; i < localStorage.length; i++) {
-        let value = localStorage.getItem(JSON.parse(i.toLocaleString()))
-        allData[i] = value
-      }
-    }
-  }
-  // @ts-ignore
-  const data = JSON.parse(localStorage.getItem('advertise-1'))
-
   return (
     <section className="flex items-start gap-10 p-10">
       <BasicDetailsForm onSave={storeValues} />
-      {data && data.category === 'sell' ? (
-        <PropertyDetailsForm onSave={storeValues} />
-      ) : (
-        data && data.category === 'rent' && <RentPropertyDetailsForm onSave={storeValues} />
-      )}
+      <PropertyDetailsForm onSave={storeValues} />
+      <RentPropertyDetailsForm onSave={storeValues} />
       <LocationDetailsForm onSave={storeValues} />
       <AmenitiesForm onSave={storeValues} />
       <ProjectStatusForm onSave={storeValues} />
