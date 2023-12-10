5a1e8169072e4fdac05dacf5bc6a4e063b5c359a
codezera <codezera3@gmail.com>
2023-12-10 15:10:21 +0530

    moved svg to separate component

--- src/app/(application)/mortgage/[step]/page.tsx
+++ src/app/(application)/mortgage/[step]/page.tsx
@@ -6,6 +6,7 @@ import { usePathname, useSearchParams } from 'next/navigation'
 import React from 'react'
 import IncomeDetailsForm from '../_form/income-details-form';
 import PersonalDetailsForm from '../_form/personal-details-form';
+import BoxStrokesIcon from '@/components/svgs/box-strokes';
 
 const Page = () => {
 
@@ -29,35 +30,7 @@ const Page = () => {
                     backgroundColor: `rgb(91, 55, 118)`,
                 }}
             ></div>
-            <svg
-                className="absolute inset-0 -z-10 h-full w-full stroke-gray-400 [mask-image:radial-gradient(100%_100%_at_top_right,white,transparent)]"
-                aria-hidden="true"
-            >
-                <defs>
-                    <pattern
-                        id="83fd4e5a-9d52-42fc-97b6-718e5d7ee527"
-                        width="200"
-                        height="200"
-                        x="50%"
-                        y="-1"
-                        patternUnits="userSpaceOnUse"
-                    >
-                        <path d="M100 200V.5M.5 .5H200" fill="none"></path>
-                    </pattern>
-                </defs>
-                <svg x="50%" y="-1" className="overflow-visible fill-gray-600">
-                    <path
-                        d="M-100.5 0h201v201h-201Z M699.5 0h201v201h-201Z M499.5 400h201v201h-201Z M-300.5 600h201v201h-201Z"
-                        strokeWidth="0"
-                    ></path>
-                </svg>
-                <rect
-                    width="100%"
-                    height="100%"
-                    strokeWidth="0"
-                    fill="url(#83fd4e5a-9d52-42fc-97b6-718e5d7ee527)"
-                ></rect>
-            </svg>
+            <BoxStrokesIcon />
 
             <div className="px-6 py-24 mx-auto max-w-7xl sm:py-32 lg:flex lg:items-start lg:gap-x-10 lg:px-8 lg:py-40">
                 <div className="max-w-2xl mx-auto lg:mx-0 lg:flex-shrink">
