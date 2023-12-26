5a5f7fe57d9ad05fc8ce4482ea93a42ca699c931
personal <mohammadfaisal1011@gmail.com>
2023-12-26 14:44:04 +0600

    Updated dependecies

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,4 +1,4 @@
-"use client"
+'use client'
 
 import { Package } from 'lucide-react'
 import Link from 'next/link'
@@ -7,20 +7,28 @@ import SideNavBar from '@/components/navigation/side-nav-bar'
 import { UserRoleEnum } from '@/constants/enums'
 import { PageRoutes } from '@/constants/page-routes'
 import { usePathname } from 'next/navigation'
-import { CardHeader, CardContent, Card } from "@/components/ui/card"
-import { Button } from "@/components/ui/button"
-import { Badge } from "@/components/ui/badge"
+import { CardHeader, CardContent, Card } from '@/components/ui/card'
+import { Button } from '@/components/ui/button'
+import { Badge } from '@/components/ui/badge'
 import { User } from '@/constants/types'
 import { LocalStorageKeys } from '@/constants/local-storage-keys'
 
 const roleToPageMapping = {
-  [UserRoleEnum.ADMIN]: [PageRoutes.admin.AGENTS, PageRoutes.admin.USERS, PageRoutes.admin.REQUIREMENTS, PageRoutes.dashboard.PROFILE],
-  [UserRoleEnum.ADVERTISER]: [PageRoutes.dashboard.MORTGAGES, PageRoutes.dashboard.PROPERTIES, PageRoutes.dashboard.PROFILE]
+  [UserRoleEnum.ADMIN]: [
+    PageRoutes.admin.AGENTS,
+    PageRoutes.admin.USERS,
+    PageRoutes.admin.REQUIREMENTS,
+    PageRoutes.dashboard.PROFILE
+  ],
+  [UserRoleEnum.ADVERTISER]: [
+    PageRoutes.dashboard.MORTGAGES,
+    PageRoutes.dashboard.PROPERTIES,
+    PageRoutes.dashboard.PROFILE
+  ]
 }
 
 export default function AdminLayout({ children }: { children: React.ReactNode }) {
-
-  const pathName = usePathname();
+  const pathName = usePathname()
 
   const storedValue = localStorage.getItem(LocalStorageKeys.USER)
 
@@ -28,11 +36,11 @@ export default function AdminLayout({ children }: { children: React.ReactNode })
 
   if (!user) {
     return (
-      <main className='flex flex-col items-center justify-center h-screen bg-gray-100 space-y-10'>
+      <main className="flex h-screen flex-col items-center justify-center space-y-10 bg-gray-100">
         <div className="w-[300px]">
           <Card>
             <CardHeader>
-              <h2 className="text-2xl font-bold text-center">Welcome!</h2>
+              <h2 className="text-center text-2xl font-bold">Welcome!</h2>
             </CardHeader>
             <CardContent>
               <p className="text-center text-gray-600">You&apos;re not currently signed in</p>
@@ -46,7 +54,7 @@ export default function AdminLayout({ children }: { children: React.ReactNode })
         </div>
         <div className="flex items-center gap-2">
           <Badge variant="secondary">New User?</Badge>
-          <Button className="text-primary border-black hover:bg-primary hover:text-white" variant="outline">
+          <Button className="border-black text-primary hover:bg-primary hover:text-white" variant="outline">
             <Link href="/signup">Sign Up</Link>
           </Button>
         </div>
@@ -55,14 +63,14 @@ export default function AdminLayout({ children }: { children: React.ReactNode })
   }
 
   // @ts-ignore
-  const allowedPages = roleToPageMapping[user.role];
+  const allowedPages = roleToPageMapping[user.role]
 
   if (!allowedPages?.some((page: string) => pathName.startsWith(page))) {
     return (
       <div className="flex h-screen flex-col items-center justify-center">
         <Package size={200} />
         <h1 className="text-3xl font-bold">You are not allowed to access this page</h1>
-        <Link className='text-primary underline' href={'/'}>
+        <Link className="text-primary underline" href={'/'}>
           Go to home page
         </Link>
       </div>
