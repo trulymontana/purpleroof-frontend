a404b10db799eb7be4d1abc72b21745b322151ed
personal <mohammadfaisal1011@gmail.com>
2024-01-06 23:09:40 +0600

    rename

--- src/app/(application)/advertise/_form/index.ts
+++ src/app/(application)/advertise/_form/index.ts
@@ -1,11 +1,11 @@
-import AmenitiesForm from './AmenitiesForm'
-import BasicDetailsForm from './BasicDetailsForm'
-import LocationDetailsForm from './LocationDetailsForm'
-import UploadDocumentsForm from './UploadDocumentsForm'
-import ProjectStatusForm from './ProjectStatusForm'
-import PropertyDetailsForm from './PropertyDetailsForm'
-import RentPropertyDetailsForm from './RentPropertyDetailsForm'
-import CallPreferenceForm from './CallPreferenceForm'
+import AmenitiesForm from './amenities-form'
+import BasicDetailsForm from './basic-details-form'
+import LocationDetailsForm from './location-details-form'
+import UploadDocumentsForm from './upload-documents-form'
+import ProjectStatusForm from './project-status-form'
+import PropertyDetailsForm from './property-details-form'
+import RentPropertyDetailsForm from './rent-property-details-form'
+import CallPreferenceForm from './call-preference-form'
 
 export {
   AmenitiesForm,
