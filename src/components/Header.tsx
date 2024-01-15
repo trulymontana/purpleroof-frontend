32f50239eef2714ca49327ff37ad17c334a05804
codezera <codezera3@gmail.com>
2024-01-15 17:56:46 +0530

    updated header link

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@
 import { ArrowRight, Menu } from 'lucide-react'
 import Link from 'next/link'
 import { Button } from './ui/button'
-import { headerLinks } from '@/constants/navigation'
+import { headerLinks, otherLinks } from '@/constants/navigation'
 import Image from 'next/image'
 import {
   Sheet,
@@ -18,8 +18,8 @@ import { PageRoutes } from '@/constants/page-routes'
 
 const Header = () => {
   return (
-    <nav className="flex items-center justify-between bg-primary/80 px-10 py-5">
-      <Link href="/" className="flex items-center space-x-2">
+    <nav className="flex items-center justify-between bg-primary/90 px-10 py-5">
+      <Link href={otherLinks.PURPLEROOF} className="flex items-center space-x-2">
         <Image
           src={'/assets/logos/logo-only-white.png'}
           quality={100}
