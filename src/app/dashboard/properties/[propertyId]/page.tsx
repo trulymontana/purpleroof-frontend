8f2bb494e3eb6b4c6fea19e63d50cda192dd3c47
codezera <codezera3@gmail.com>
2024-01-24 20:58:11 +0530

    removed carousel for now

--- src/app/dashboard/properties/[propertyId]/page.tsx
+++ src/app/dashboard/properties/[propertyId]/page.tsx
@@ -27,53 +27,36 @@ const Page = ({ params: { propertyId } }: Props) => {
 
   return (
     <div className='overflow-hidden'>
-      <Dialog>
-        <DialogTrigger asChild>
-          <section className="cursor-pointer h-[500px] w-full flex px-2 py-3 gap-4 border-2 rounded-lg mx-2 my-4">
-            <div className='flex-1'>
-              <Image
-                alt="Property Image"
-                className="h-full w-full object-cover"
-                height="1000"
-                src={data?.image || '/placeholder.svg'}
-                style={{
-                  aspectRatio: '1000/500',
-                  objectFit: 'contain'
-                }}
-                width="1000"
-                priority
-                quality={100}
-              />
-            </div>
-            <div className='w-1/4 flex flex-col gap-4 overflow-y-scroll'>
-              {data?.photos.map((photo: any, i: number) => (
-                <Image
-                  key={i}
-                  alt="Property Image"
-                  className="h-full w-full object-cover"
-                  src={photo?.name || '/placeholder.svg'}
-                  height="300"
-                  width="500"
-                  quality={100}
-                />
-              ))}
-            </div>
-          </section>
-        </DialogTrigger>
-        <DialogContent>
-          <Carousel>
-            <CarouselContent>
-              <CarouselItem>...</CarouselItem>
-              <CarouselItem>...</CarouselItem>
-              <CarouselItem>...</CarouselItem>
-            </CarouselContent>
-            <CarouselPrevious />
-            <CarouselNext />
-          </Carousel>
-        </DialogContent>
-      </Dialog>
-
-
+      <section className="cursor-pointer h-[500px] w-full flex px-2 py-3 gap-4 border-2 rounded-lg mx-2 my-4">
+        <div className='flex-1'>
+          <Image
+            alt="Property Image"
+            className="h-full w-full object-cover"
+            height="1000"
+            src={data?.image || '/placeholder.svg'}
+            style={{
+              aspectRatio: '1000/500',
+              objectFit: 'contain'
+            }}
+            width="1000"
+            priority
+            quality={100}
+          />
+        </div>
+        <div className='w-1/4 flex flex-col gap-4 overflow-y-scroll'>
+          {data?.photos.map((photo: any, i: number) => (
+            <Image
+              key={i}
+              alt="Property Image"
+              className="h-full w-full object-cover"
+              src={photo?.name || '/placeholder.svg'}
+              height="300"
+              width="500"
+              quality={100}
+            />
+          ))}
+        </div>
+      </section>
       <div className="w-full">
         <div className="mx-auto flex max-w-[90rem] items-start gap-8 p-6 ">
           {data && (
